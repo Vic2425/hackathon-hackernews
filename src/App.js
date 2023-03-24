6df7841e9d758c5ed69de7287761976da5322ce2
Vic <vicjs612@gmail.com>
2023-03-23 20:47:03 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { React, useState, useEffect } from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
